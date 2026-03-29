2dac3c06e6a5c1716da36078f662e67c938fae03
Eli Apeles <eli.apeles@integraerp.com.br>
2026-03-29 18:50:00 -0300

    Feat Adicionando paginação

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -6,17 +6,22 @@ public class Categories : PageModel
 {
     public List<Category> CategoryList { get; set; } = new();
 
-    public void OnGet()
-    {
-
-        for (var i = 0; i <= 100; i++)
+    public void OnGet(
+        int skip=0,
+        int take=25)
+    {   
+        var temp = new List<Category>();
+        for (var i = 0; i <= 1787; i++)
         {
-            CategoryList.Add(
+            temp.Add(
                 new Category(i,
                     $"Categoria {i}",
                     i * 18.95M));
         }
-
+        CategoryList = temp
+            .Skip(skip)
+            .Take(take)
+            .ToList();
     }
 }
 
--- obj/Debug/net10.0/MyRazorApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e0f35086e0825099e8f00e55ef13827d04e3b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a35c5fc77e020a2cdec8d9cce3f8ffa9410f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
